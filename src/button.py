45e5677dd8bef3a560f8f0b29b59cbde12771ca2
Dylan <Dylanmwakasekele@gmai.com>
2024-01-23 11:49:40 +0000

    ready to complete button.py

--- src/button.py
+++ src/button.py
@@ -4,11 +4,10 @@ import machine, time
 #Set up compinents/global variables
 ledPin = 15
 led = machine.Pin(ledPin, machine.Pin.OUT)
+button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 #Start infinite loop to run program
 while True:
     #Main program code
-    led.high()
-    time.sleep(0.5)
-    led.low()
-    time.sleep(0.5)
+    print(button.value())
+    time.sleep(1)
