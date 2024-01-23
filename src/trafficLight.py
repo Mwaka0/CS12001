919ef70c26b5fec99adec7cb21ea507e476e363f
Dylan <Dylanmwakasekele@gmai.com>
2024-01-23 11:02:46 +0000

    Completed the traffic light task

--- src/trafficLight.py
+++ src/trafficLight.py
@@ -7,11 +7,20 @@ red = 15
 yellow = 14
 green = 13
 
-while True:
-    toggleLED(red)
-    time.sleep(0.5)
-
-
-def toggleLED(colour):
+def toggleLED(ledPin):
     led = machine.Pin(ledPin, machine.Pin.OUT)
     led.toggle()
+
+while True:
+    toggleLED(red)
+    time.sleep(0.75)
+    toggleLED(yellow)
+    time.sleep(0.75)
+    toggleLED(red)
+    toggleLED(yellow)
+    toggleLED(green)
+    time.sleep(0.75)
+    toggleLED(green)
+    toggleLED(yellow)
+    time.sleep(0.75)
+    toggleLED(yellow)
