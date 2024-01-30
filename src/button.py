f0edca8a39093c09d864ac014389c059fb7fe0c4
Dylan <Dylanmwakasekele@gmai.com>
2024-01-30 11:40:02 +0000

    Button works!!!

--- src/button.py
+++ src/button.py
@@ -2,12 +2,14 @@
 import machine, time
 
 #Set up compinents/global variables
-ledPin = 15
+ledPin = 16
 led = machine.Pin(ledPin, machine.Pin.OUT)
-button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 #Start infinite loop to run program
 while True:
     #Main program code
-    print(button.value())
-    time.sleep(1)
+    if button.value(): 
+        led.high()
+    else:
+        led.low()
